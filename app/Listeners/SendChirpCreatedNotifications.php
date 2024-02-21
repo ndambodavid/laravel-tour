fcd73f342cb4a183ff00f81faf3a4de15fa58fe9
ndambodavid <linmik420@gmail.com>
2024-02-21 12:26:55 +0300

    notifications and events

---
+++ app/Listeners/SendChirpCreatedNotifications.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\ChirpCreated;
+use App\Models\User;
+use App\Notifications\NewChirp;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+
+class SendChirpCreatedNotifications implements ShouldQueue
+{
+    /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     */
+    public function handle(ChirpCreated $event): void
+    {
+        foreach (User::WhereNot('email', $event->chirp->user->email)->cursor() as $user) {
+            $user->notify(new NewChirp($event->chirp));
+        }
+    }
+}
